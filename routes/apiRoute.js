6e8d11deb44cadf7d0a993bf3247acbec8a1cc0c
dylanstoudt <138310076+dylanstoudt@users.noreply.github.com>
2023-08-24 15:19:25 -0400

    created get and post request for notes api, currently not working, believe it is a routing issue, going to complete delete request and attempt to fix routing

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -9,5 +9,22 @@ app.get('/api/notes', (req, res) => {
   });
 
 app.post('/api/notes', (req, res) => {
+    // Sets database data as a variable
     let db = fs.readFile('db/db.json')
-})
+    // Parses db data
+    db = json.parse(db)
+    res.json(db);
+
+    // Creates a blank variable for new userNote
+    let userNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uniqid()
+    };
+    // Pushes new userNote into db and reparse
+    db.push(userNote)
+    fs.writeFile('db/db.json'), JSON.stringify(db);
+    res.json(db)
+});
+
+module.exports = app
